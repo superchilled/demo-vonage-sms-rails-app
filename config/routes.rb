14b78fb172e3c6c093f56fe394ef25ee95c30d8e
superchilled <karl@superchilled.co.uk>
2022-03-09 14:12:24 +0000

    Defining routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "sms#show"
+
+  get '/sms', to: 'sms#show'
+  post '/sms', to: 'sms#send_sms'
 end
