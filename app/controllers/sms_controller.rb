1e76923c780afb2753d53790e16f6f3f7ea9e301
superchilled <karl@superchilled.co.uk>
2022-03-09 15:18:20 +0000

    Defining send_sms controller action

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,3 +1,13 @@
 class SmsController < ApplicationController
   def show; end
+
+  def send_sms
+    number = params[:number]
+    message = params[:message]
+    client = Vonage::Client.new(api_key: ENV['VONAGE_API_KEY'], api_secret: ENV['VONAGE_API_SECRET'])
+
+    client.sms.send(from: 'Vonage', to: number, text: message)
+
+    redirect_to '/sms', notice: "Your message was sent"
+  end
 end
