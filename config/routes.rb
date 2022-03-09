69473dfd5b40ce370b6bdf26a87944f896fb7e28
superchilled <karl@superchilled.co.uk>
2022-03-09 15:48:15 +0000

    Redirecting root path to /sms

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "sms#show"
+  root :to => redirect('/sms')
 
   get '/sms', to: 'sms#show'
   post '/sms', to: 'sms#send_sms'
